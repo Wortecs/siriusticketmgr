c68ca975202c94186c9a9c4b8ab5519511b646d4
Horse <skostians@mail.ru>
2016-05-29 23:32:43 +0300

    Base server and handler

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func mainHandler(c *gin.Context) {
+	file, err := ioutil.ReadFile("static/main.html")
+	if err != nil {
+		c.String(http.StatusBadRequest, "Error!!!")
+	}
+	c.Writer.Write(file)
+}
 
 func main() {
+
+	router := gin.Default()
+	router.GET("/", mainHandler)
+	router.Run(":8080")
 	fmt.Println("Hello world")
 }
